201f7867e4337cb1bd6c099f62e7e912c25199c2
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-09-26 22:44:19 -0300

    feat: add service

--- src/main/java/org/example/paymentapi/repository/PaymentRepository.java
+++ src/main/java/org/example/paymentapi/repository/PaymentRepository.java
@@ -3,9 +3,10 @@ package org.example.paymentapi.repository;
 import org.example.paymentapi.model.PaymentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<PaymentModel, UUID>{
-    PaymentModel findByCardNumber (String cardNumber);
-
+    Optional<PaymentModel> findByCardNumber (String cardNumber);
+    Optional<PaymentModel> findByCardHolderName (String cardHolderName);
 }
--- src/main/java/org/example/paymentapi/service/PaymentService.java
+++ src/main/java/org/example/paymentapi/service/PaymentService.java
@@ -19,6 +19,34 @@ public class PaymentService {
 
     public PaymentResponseDTO findPaymentById(UUID id) {
        PaymentModel paymentModel = paymentRepository.findById(id).orElseThrow(() -> new RuntimeException("Payment not found"));
-         return modelMapper.map(paymentModel, PaymentResponseDTO.class);
+       return modelMapper.map(paymentModel, PaymentResponseDTO.class);
     }
+
+    public PaymentResponseDTO findPaymentByCardNumber(String cardNumber) {
+        PaymentModel paymentModel = paymentRepository.findByCardNumber(cardNumber).orElseThrow(() -> new RuntimeException("Payment not found"));
+        return modelMapper.map(paymentModel, PaymentResponseDTO.class);
+    }
+
+    public PaymentResponseDTO savePayment(PaymentModel paymentModel) {
+        PaymentModel savedPayment = paymentRepository.save(paymentModel);
+        if(savedPayment == null) {
+            throw new RuntimeException("Payment not saved");
+        }
+        return modelMapper.map(savedPayment, PaymentResponseDTO.class);
+    }
+
+    public PaymentResponseDTO updatePayment(UUID id, PaymentModel paymentModel) {
+        PaymentModel updatedPayment = paymentRepository.save(paymentModel);
+        return modelMapper.map(updatedPayment, PaymentResponseDTO.class);
+    }
+
+    public void deletePayment(UUID id) {
+        paymentRepository.deleteById(id);
+    }
+
+    public PaymentResponseDTO findPaymentByCardHolderName(String cardHolderName) {
+        PaymentModel paymentModel = paymentRepository.findByCardHolderName(cardHolderName).orElseThrow(() -> new RuntimeException("Payment not found"));
+        return modelMapper.map(paymentModel, PaymentResponseDTO.class);
+    }
+
 }
